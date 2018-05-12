df7be89d6fb0caf64b7f6bae3dcf790d3fb2c6b4
StrayBird_ATSH <16307110064@fudan.edu.cn>
2018-05-12 20:59:17 +0800

    Makes major change to totally refactor the whole project to make it better adapted to Bootstrap libraries and easy to maintain

--- js/collapse-menu.js
+++
@@ -1,8 +0,0 @@
-var parentElement = document.getElementsByClassName('dropdown');
-var element = document.getElementsByClassName('dropdown-menu');
-
-parentElement[0].onclick = function () {
-    element[0].style.display === 'none' ?
-        element[0].style.display = 'block' :
-        element[0].style.display = 'none';
-};
--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,3 @@
-var parentElement = document.getElementsByClassName('dropdown');
-var element = document.getElementsByClassName('dropdown-menu');
-
-parentElement[0].onclick = function () {
-    element[0].style.display === 'none' ?
-        element[0].style.display = 'block' :
-        element[0].style.display = 'none';
-};
-
 function changeImage() {
     var currentElement = document.getElementsByClassName('slick-current');
     var id = currentElement[0].id;
